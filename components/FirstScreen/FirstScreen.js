94677d2ff92b4be2a9ce6ef8ef5cf8f1c34b2206
eleskin <eleskin1201@gmail.com>
2022-04-02 13:00:00 +0300

    feature: Added FirstScreen component

---
+++ components/FirstScreen/FirstScreen.js
@@ -0,0 +1,11 @@
+import styles from './FirstScreen.module.scss';
+
+const FirstScreen = () => {
+	return (
+		<div className={styles.FirstScreen}>
+		
+		</div>
+	);
+};
+
+export default FirstScreen;
