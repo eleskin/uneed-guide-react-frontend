b868a89614c1d41732d64a69f5939a479f1a38a3
eleskin <eleskin1201@gmail.com>
2022-04-02 17:01:33 +0300

    fix: Removed unused code

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,8 @@
 import styles from '/styles/Home.module.scss';
-import Button from '../components/Button/Button';
 
 const Home = () => {
 	return (
 		<div className={styles.Home}>
-			<Button.Primary small={true}>Нет, другой</Button.Primary>
-			<Button.Secondary small={true}>Нет, другой</Button.Secondary>
-			<Button.Outlined small={true}>Нет, другой</Button.Outlined>
-			<Button.Primary small={false}>Нет, другой</Button.Primary>
-			<Button.Secondary small={false}>Нет, другой</Button.Secondary>
-			<Button.Outlined small={false}>Нет, другой</Button.Outlined>
 		</div>
 	);
 };
