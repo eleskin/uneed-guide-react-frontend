778c6171a1fa717142f038579040a441858e9890
eleskin <eleskin1201@gmail.com>
2022-04-02 11:13:13 +0300

    feature: Add Menu search

--- components/Menu/Menu.js
+++ components/Menu/Menu.js
@@ -1,5 +1,6 @@
 import Button from '../Button/Button';
 import styles from './Menu.module.scss';
+import Image from 'next/image';
 
 const Menu = () => {
 	return (
@@ -8,6 +9,15 @@ const Menu = () => {
 				<Button.Secondary>Авторизация</Button.Secondary>
 				<Button.Primary>Регистрация</Button.Primary>
 			</header>
+			<div className={styles.Menu__search}>
+				<label>
+					<input type="text" placeholder="Например, экскурсии по Москва-реке"/>
+					<Image src="/assets/images/header/header-magnifier.svg" width={16} height={16}/>
+				</label>
+				<span>
+					Последние запросы: <b>водные экскурсии, экскурсия в музее</b>
+				</span>
+			</div>
 		</div>
 	);
 };
