24fb49832feaf30b6b3e3ce51bec24af477f33f8
eleskin <eleskin1201@gmail.com>
2022-04-02 10:49:13 +0300

    feature: Add Menu header

--- components/Menu/Menu.js
+++ components/Menu/Menu.js
@@ -1,9 +1,13 @@
+import Button from '../Button/Button';
 import styles from './Menu.module.scss';
 
 const Menu = () => {
 	return (
 		<div className={styles.Menu}>
-		
+			<header className={styles.Menu__header}>
+				<Button.Secondary>Авторизация</Button.Secondary>
+				<Button.Primary>Регистрация</Button.Primary>
+			</header>
 		</div>
 	);
 };
--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import '/styles/globals.scss';
 import Head from 'next/head';
 import {Fragment} from 'react';
 import Header from '../components/Header/Header';
+import Menu from '../components/Menu/Menu';
 
 const App = ({Component, pageProps}) => {
 	return (
@@ -11,6 +12,7 @@ const App = ({Component, pageProps}) => {
 				<title>Uneed Guide</title>
 			</Head>
 			<Header/>
+			<Menu/>
 			<Component {...pageProps} />
 		</Fragment>
 	);
