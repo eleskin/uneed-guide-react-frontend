46e5fd0b4a940e8bbe64edb36a79cbb9ec941caf
eleskin <eleskin1201@gmail.com>
2022-04-02 11:24:21 +0300

    feature: Add Menu navigation

--- components/Menu/Menu.js
+++ components/Menu/Menu.js
@@ -1,6 +1,7 @@
 import Button from '../Button/Button';
 import styles from './Menu.module.scss';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Menu = () => {
 	return (
@@ -18,6 +19,47 @@ const Menu = () => {
 					Последние запросы: <b>водные экскурсии, экскурсия в музее</b>
 				</span>
 			</div>
+			<nav className={styles.Menu__nav}>
+				<h6>Навигация</h6>
+				<ul>
+					<li>
+						<Link href="#"><a>Каталог экскурсий</a></Link>
+					</li>
+					<li>
+						<Link href="#"><a>Условия оплаты и возврата</a></Link>
+					</li>
+					<li>
+						<Link href="#"><a>Частозадаваемые вопросы</a></Link>
+					</li>
+					<li>
+						<Link href="#"><a>Блог</a></Link>
+					</li>
+					<li>
+						<Link href="#"><a>Контакты</a></Link>
+					</li>
+				</ul>
+				<h6>Каталог экскурсий</h6>
+				<ul>
+					<li>
+						<Link href="#"><a>Дневные экскурсии</a></Link>
+					</li>
+					<li>
+						<Link href="#"><a>Метеоры</a></Link>
+					</li>
+					<li>
+						<Link href="#"><a>Ночные экскурсии</a></Link>
+					</li>
+					<li>
+						<Link href="#"><a>Крепость Орешек</a></Link>
+					</li>
+					<li>
+						<Link href="#"><a>Праздники</a></Link>
+					</li>
+					<li>
+						<Link href="#"><a>Петергоф</a></Link>
+					</li>
+				</ul>
+			</nav>
 		</div>
 	);
 };
