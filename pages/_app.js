93ec5722788b7459f9e1127f61581e1be1bbc011
eleskin <eleskin1201@gmail.com>
2022-04-04 12:08:28 +0300

    feature: Added showing FirstScreen

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,16 @@
 import '/styles/normalize.scss';
 import '/styles/globals.scss';
 import Head from 'next/head';
-import {Fragment, useState} from 'react';
-import Header from '../components/Header/Header';
-import Menu from '../components/Menu/Menu';
+import {Fragment} from 'react';
+import FirstScreen from '../components/FirstScreen/FirstScreen';
 
 const App = ({Component, pageProps}) => {
-	const [isActiveMenu, setIsActiveMenu] = useState(false);
-	const [headerHeight, setHeaderHeight] = useState(0);
-	
 	return (
 		<Fragment>
 			<Head>
 				<title>Uneed Guide</title>
 			</Head>
-			<style jsx global>{`body {overflow: ${isActiveMenu ? 'hidden' : 'auto '}}`}</style>
-			<Header isActiveMenu={isActiveMenu} setIsActiveMenu={setIsActiveMenu} setHeaderHeight={setHeaderHeight}/>
-			<Menu isActiveMenu={isActiveMenu} style={{height: `calc(100% - ${headerHeight}px)`}}/>
+			<FirstScreen/>
 			<Component {...pageProps} />
 		</Fragment>
 	);
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
 import styles from '/styles/Home.module.scss';
-import FirstScreen from '../components/FirstScreen/FirstScreen';
 
 const Home = () => {
 	return (
 		<div className={styles.Home}>
-			<FirstScreen/>
 		</div>
 	);
 };
