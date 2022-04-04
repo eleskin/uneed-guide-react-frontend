281cd51daf06f515728bba72df37e94594ca6498
eleskin <eleskin1201@gmail.com>
2022-04-04 11:55:30 +0300

    feature: Added showing Menu

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,16 @@
 import '/styles/normalize.scss';
 import '/styles/globals.scss';
 import Head from 'next/head';
-import {Fragment, useState} from 'react';
+import {Fragment} from 'react';
 import Menu from '../components/Menu/Menu';
 
 const App = ({Component, pageProps}) => {
-	const [isActiveMenu,] = useState(false);
-	const [headerHeight,] = useState(0);
-	
 	return (
 		<Fragment>
 			<Head>
 				<title>Uneed Guide</title>
 			</Head>
-			<style jsx global>{`body {overflow: ${isActiveMenu ? 'hidden' : 'auto '}}`}</style>
-			<Menu isActiveMenu={isActiveMenu} style={{height: `calc(100% - ${headerHeight}px)`}}/>
+			<Menu isActiveMenu={true}/>
 			<Component {...pageProps} />
 		</Fragment>
 	);
