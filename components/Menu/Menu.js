3e982d9da9c302fd204fe1f135e3b1d05950283c
eleskin <eleskin1201@gmail.com>
2022-04-04 15:25:13 +0300

    feature: Added Menu component

---
+++ components/Menu/Menu.js
@@ -0,0 +1,72 @@
+import Button from '../Button/Button';
+import styles from './Menu.module.scss';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Menu = ({isActiveMenu, ...props}) => {
+	return (
+		<div {...props} className={`${styles.Menu} ${isActiveMenu ? styles.Menu_active : ''}`}>
+			<header className={styles.Menu__header}>
+				<Button.Secondary>Авторизация</Button.Secondary>
+				<Button.Primary>Регистрация</Button.Primary>
+			</header>
+			<div className={styles.Menu__search}>
+				<label>
+					<input type="text" placeholder="Например, экскурсии по Москва-реке"/>
+					<Image
+						src="/assets/images/menu/menu-magnifier.svg"
+						width={16}
+						height={16}
+						alt=""
+					/>
+				</label>
+				<span>
+					Последние запросы: <b>водные экскурсии, экскурсия в музее</b>
+				</span>
+			</div>
+			<nav className={styles.Menu__nav}>
+				<h6>Навигация</h6>
+				<ul>
+					<li>
+						<Link href="#"><a>Каталог экскурсий</a></Link>
+					</li>
+					<li>
+						<Link href="#"><a>Условия оплаты и возврата</a></Link>
+					</li>
+					<li>
+						<Link href="#"><a>Частозадаваемые вопросы</a></Link>
+					</li>
+					<li>
+						<Link href="#"><a>Блог</a></Link>
+					</li>
+					<li>
+						<Link href="#"><a>Контакты</a></Link>
+					</li>
+				</ul>
+				<h6>Каталог экскурсий</h6>
+				<ul>
+					<li>
+						<Link href="#"><a>Дневные экскурсии</a></Link>
+					</li>
+					<li>
+						<Link href="#"><a>Метеоры</a></Link>
+					</li>
+					<li>
+						<Link href="#"><a>Ночные экскурсии</a></Link>
+					</li>
+					<li>
+						<Link href="#"><a>Крепость Орешек</a></Link>
+					</li>
+					<li>
+						<Link href="#"><a>Праздники</a></Link>
+					</li>
+					<li>
+						<Link href="#"><a>Петергоф</a></Link>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	);
+};
+
+export default Menu;
