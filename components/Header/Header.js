a0372904be0acb0bf4f9d4efdd30b51695d1c77b
eleskin <eleskin1201@gmail.com>
2022-04-04 23:29:18 +0300

    feature: Added Header desktop elements

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,4 +1,5 @@
 import {createRef, useEffect} from 'react';
+import Button from '../Button/Button';
 import styles from './Header.module.scss';
 import Link from 'next/link';
 import Image from 'next/image';
@@ -30,20 +31,29 @@ const Header = ({isActiveMenu, setIsActiveMenu, setHeaderHeight, ...props}) => {
 			className={`${styles.Header} ${isActiveMenu ? styles.Header_active : ''}`}
 			ref={headerRef}
 		>
-			<Link href="/">
-				<a className={styles.Header__logo}>
-					<Image
-						src="/assets/images/header/header-logo.svg"
-						width={30}
-						height={36}
-						alt=""
-					/>
-				</a>
-			</Link>
-			<button
-				className={styles.Header__button}
-				onClick={handleButtonClick}
-			/>
+			<div className={styles.Header__container}>
+				<Link href="/">
+					<a className={styles.Header__logo}>
+						<Image
+							src="/assets/images/header/header-logo.svg"
+							width={30}
+							height={36}
+							alt=""
+						/>
+					</a>
+				</Link>
+				<div className={styles.Header__desktop}>
+					<div className={styles.Header__center}>
+						<Button.Outlined onClick={handleButtonClick}>Каталог</Button.Outlined>
+						<input type="text"/>
+					</div>
+					<Button.Outlined>Вход или регистрация</Button.Outlined>
+				</div>
+				<button
+					className={styles.Header__button}
+					onClick={handleButtonClick}
+				/>
+			</div>
 		</header>
 	);
 };
