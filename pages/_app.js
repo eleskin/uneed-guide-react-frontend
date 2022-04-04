6a209cc8167d5b20a4cae106cfbe09fca8919d36
eleskin <eleskin1201@gmail.com>
2022-04-04 11:30:13 +0300

    fix: Removed unused components

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,9 @@
 import '/styles/normalize.scss';
 import '/styles/globals.scss';
-import Head from 'next/head';
-import {Fragment} from 'react';
-import Header from '../components/Header/Header';
 
 const App = ({Component, pageProps}) => {
 	return (
-		<Fragment>
-			<Head>
-				<title>Uneed Guide</title>
-			</Head>
-			<Header/>
-			<Component {...pageProps} />
-		</Fragment>
+		<Component {...pageProps} />
 	);
 };
 
