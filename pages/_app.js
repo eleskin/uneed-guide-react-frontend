1bfd38a90d7b349de94f9af33a2f238cd305940a
eleskin <eleskin1201@gmail.com>
2022-04-04 11:52:51 +0300

    feature: Added showing Buttons

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,30 @@
 import '/styles/normalize.scss';
 import '/styles/globals.scss';
+import {Fragment} from 'react';
+import Button from '../components/Button/Button';
 
-const App = ({Component, pageProps}) => {
+const App = () => {
 	return (
-		<Component {...pageProps} />
+		<Fragment>
+			<div>
+				<Button.Primary>Primary Button</Button.Primary>
+			</div>
+			<div>
+				<Button.Outlined>Outlined Button</Button.Outlined>
+			</div>
+			<div>
+				<Button.Secondary>Secondary Button</Button.Secondary>
+			</div>
+			<div>
+				<Button.Primary small={true}>Primary Button</Button.Primary>
+			</div>
+			<div>
+				<Button.Outlined small={true}>Outlined Button</Button.Outlined>
+			</div>
+			<div>
+				<Button.Secondary small={true}>Secondary Button</Button.Secondary>
+			</div>
+		</Fragment>
 	);
 };
 
