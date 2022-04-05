f48eb4016269ab481955cda440a7a36ac65a2d9b
eleskin <eleskin1201@gmail.com>
2022-04-05 16:47:22 +0300

    fix: Adaptive for large screens

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -5,7 +5,16 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 const Header = ({isActiveMenu, setIsActiveMenu, setHeaderHeight, ...props}) => {
-	const [isLogin,] = useState(false);
+	const [isLogin] = useState(false);
+	const [isVisibleRegionSelector, setIsVisibleRegionSelector] = useState(true);
+	
+	const handleButtonCloseClick = () => {
+		setIsVisibleRegionSelector(false);
+	};
+	
+	const handleButtonOpenClick = () => {
+		setIsVisibleRegionSelector(true);
+	};
 	
 	const handleButtonClick = () => {
 		setTimeout(() => setIsActiveMenu(!isActiveMenu), 0);
@@ -17,7 +26,7 @@ const Header = ({isActiveMenu, setIsActiveMenu, setHeaderHeight, ...props}) => {
 		if (headerRef.current instanceof Element) {
 			setHeaderHeight(parseFloat(window.getComputedStyle(headerRef.current).height));
 		}
-	}, []);
+	}, [headerRef, setHeaderHeight]);
 	
 	if (typeof window !== 'undefined') {
 		window.addEventListener('resize', () => {
@@ -33,9 +42,67 @@ const Header = ({isActiveMenu, setIsActiveMenu, setHeaderHeight, ...props}) => {
 			className={`${styles.Header} ${isActiveMenu ? styles.Header_active : ''}`}
 			ref={headerRef}
 		>
-			<div className={styles.Header__container}>
-				<Link href="#">
-					<a>
+			<div className={styles.Header__top}>
+				<div className={styles.Header__container}>
+					<div className={styles.Header__location}>
+						<div onClick={handleButtonOpenClick}>
+							<Image
+								src="/assets/images/first-screen/first-screen-map-pin.svg"
+								width={15}
+								height={15}
+								alt=""
+							/>
+							<span>г. Москва</span>
+							<i>(Ваш регион)</i>
+						</div>
+						<div
+							className={`${styles.Header__region} ${isVisibleRegionSelector ? styles.Header__region_active : ''}`}
+						>
+							<header>
+								<Image
+									src="/assets/images/first-screen/first-screen-map-pin.svg"
+									width={15}
+									height={15}
+									alt=""
+								/>
+								<span>Ваш регион Москва?</span>
+							</header>
+							<footer>
+								<Button.Primary small={true} onClick={handleButtonCloseClick}>Да, верно</Button.Primary>
+								<Button.Outlined small={true} onClick={handleButtonCloseClick}>Нет, другой</Button.Outlined>
+							</footer>
+						</div>
+					</div>
+					<div className={styles.Header__menu}>
+						<ul>
+							<li>
+								<Link href="#"><a>Помощь</a></Link>
+							</li>
+							<li>
+								<Link href="#"><a>Условия оплаты и возврата</a></Link>
+							</li>
+							<li>
+								<Link href="#"><a>Сотрудничество</a></Link>
+							</li>
+							<li>
+								<Link href="#"><a>Контакты</a></Link>
+							</li>
+						</ul>
+						<div className={styles.Header__languages}>
+							<Link href="#">
+								<a className={`${styles.Header__language} ${styles.Header__language_active}`}>RU</a>
+							</Link>
+							<Link href="#">
+								<a className={styles.Header__language}>EN</a>
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className={styles.Header__main}>
+				<div className={styles.Header__container}>
+					<Link href="/">
+						<a>
 						<span className={styles.Header__logo}>
 							<Image
 								src="/assets/images/header/header-logo.svg"
@@ -44,7 +111,7 @@ const Header = ({isActiveMenu, setIsActiveMenu, setHeaderHeight, ...props}) => {
 								alt=""
 							/>
 						</span>
-						<span className={styles.Header__logo_desktop}>
+							<span className={styles.Header__logo_desktop}>
 							<Image
 								src="/assets/images/header/header-logo-desktop.svg"
 								width={200}
@@ -52,47 +119,48 @@ const Header = ({isActiveMenu, setIsActiveMenu, setHeaderHeight, ...props}) => {
 								alt=""
 							/>
 						</span>
-					</a>
-				</Link>
-				<div className={styles.Header__desktop}>
-					<div className={styles.Header__center}>
-						<Button.Outlined small={true} onClick={handleButtonClick}>Каталог</Button.Outlined>
-						<label>
-							<Image src="/assets/images/header/header-search.svg" width={18} height={18} alt=""/>
-							<input type="text" placeholder="Поиск по Москве"/>
-						</label>
-					</div>
-					<div className={styles.Header__right}>
-						<div className={styles.Header__links}>
-							<Link href="#">
-								<a>
-									<Image src="/assets/images/header/header-cart.svg" width={17} height={14} alt=""/>
-									<span>Корзина</span>
-								</a>
-							</Link>
-							<Link href="#">
-								<a>
-									<Image src="/assets/images/header/header-heart.svg" width={17} height={14} alt=""/>
-									<span>Избранное</span>
-								</a>
-							</Link>
-							{isLogin ? (
+						</a>
+					</Link>
+					<div className={styles.Header__desktop}>
+						<div className={styles.Header__center}>
+							<Button.Outlined small={true} onClick={handleButtonClick}>Каталог</Button.Outlined>
+							<label>
+								<Image src="/assets/images/header/header-search.svg" width={18} height={18} alt=""/>
+								<input type="text" placeholder="Поиск по Москве"/>
+							</label>
+						</div>
+						<div className={styles.Header__right}>
+							<div className={styles.Header__links}>
 								<Link href="#">
 									<a>
-										<Image src="/assets/images/header/header-profile.svg" width={17} height={14} alt=""/>
-										<span>Профиль</span>
+										<Image src="/assets/images/header/header-cart.svg" width={17} height={14} alt=""/>
+										<span>Корзина</span>
 									</a>
 								</Link>
-							) : (
-								<Button.Outlined small={true}>Вход или регистрация</Button.Outlined>
-							)}
+								<Link href="#">
+									<a>
+										<Image src="/assets/images/header/header-heart.svg" width={17} height={14} alt=""/>
+										<span>Избранное</span>
+									</a>
+								</Link>
+								{isLogin ? (
+									<Link href="#">
+										<a>
+											<Image src="/assets/images/header/header-profile.svg" width={17} height={14} alt=""/>
+											<span>Профиль</span>
+										</a>
+									</Link>
+								) : (
+									<Button.Outlined small={true}>Вход или регистрация</Button.Outlined>
+								)}
+							</div>
 						</div>
 					</div>
+					<button
+						className={styles.Header__button}
+						onClick={handleButtonClick}
+					/>
 				</div>
-				<button
-					className={styles.Header__button}
-					onClick={handleButtonClick}
-				/>
 			</div>
 		</header>
 	);
