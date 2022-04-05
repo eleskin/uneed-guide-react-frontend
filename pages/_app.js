7f39039d7cbe6e669b2338495b7f5a6bd1f369f9
eleskin <eleskin1201@gmail.com>
2022-04-05 12:51:59 +0300

    fix: Hide menu

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,18 @@
 import '/styles/normalize.scss';
 import '/styles/globals.scss';
 import Head from 'next/head';
-import {Fragment} from 'react';
+import {Fragment, useState} from 'react';
 import Menu from '../components/Menu/Menu';
 
 const App = ({Component, pageProps}) => {
+	const [isActiveMenu, setIsActiveMenu] = useState(true);
+	
 	return (
 		<Fragment>
 			<Head>
 				<title>Uneed Guide</title>
 			</Head>
-			<Menu isActiveMenu={true}/>
+			<Menu isActiveMenu={isActiveMenu} setIsActiveMenu={setIsActiveMenu}/>
 			<Component {...pageProps} />
 		</Fragment>
 	);
