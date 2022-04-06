a14eadcfbd4824572925064502484bbcc2fb83c4
eleskin <eleskin1201@gmail.com>
2022-04-06 23:22:29 +0300

    fix: Fixed errors

--- pages/_app.js
+++ pages/_app.js
@@ -1,41 +1,22 @@
 import '/styles/normalize.scss';
 import '/styles/globals.scss';
 import Head from 'next/head';
-import {Fragment, useEffect, useState} from 'react';
-import Header from '../components/Header/Header';
-import Menu from '../components/Menu/Menu';
+import {Fragment} from 'react';
+import {Provider} from 'react-redux';
+import FirstScreen from '../components/FirstScreen/FirstScreen';
+import store from '../store';
 
 const App = ({Component, pageProps}) => {
-	const [isActiveMenu, setIsActiveMenu] = useState(false);
-	const [headerHeight, setHeaderHeight] = useState(0);
-	const [windowWidth, setWindowWidth] = useState(0);
-	
-	useEffect(() => {
-		setWindowWidth(window.innerWidth);
-	}, [setWindowWidth]);
-	
-	if (typeof window !== 'undefined') {
-		window.addEventListener('resize', () => {
-			setWindowWidth(window.innerWidth);
-		});
-	}
-	
 	return (
-		<Fragment>
-			<Head>
-				<title>Uneed Guide</title>
-			</Head>
-			<style jsx global>
-				{
-					`body {
-						overflow: ${isActiveMenu && windowWidth < 768 ? 'hidden' : 'auto '}
-					}`
-				}
-			</style>
-			<Header isActiveMenu={isActiveMenu} setIsActiveMenu={setIsActiveMenu} setHeaderHeight={setHeaderHeight}/>
-			<Menu isActiveMenu={isActiveMenu} setIsActiveMenu={setIsActiveMenu} style={{height: `calc(100% - ${headerHeight}px)`}}/>
-			<Component {...pageProps} />
-		</Fragment>
+		<Provider store={store}>
+			<Fragment>
+				<Head>
+					<title>Uneed Guide</title>
+				</Head>
+				<FirstScreen/>
+				<Component {...pageProps} />
+			</Fragment>
+		</Provider>
 	);
 };
 
