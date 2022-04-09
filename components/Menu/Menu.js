38afc72e1aa7ae37d6b218b1f1b6f8c1c0be02bb
eleskin <eleskin1201@gmail.com>
2022-04-09 21:49:14 +0300

    feature: Added translation files

--- components/Menu/Menu.js
+++ components/Menu/Menu.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
-import {createRef} from 'react';
+import {useRouter} from 'next/router';
+import {createRef, useEffect, useState} from 'react';
 import {useOutsideClickHandler} from '../../utils/hooks';
 import Button from '../Button/Button';
 import Navigation from '../Navigation/Navigation';
@@ -7,32 +8,40 @@ import styles from './Menu.module.scss';
 
 const Menu = ({isActiveMenu, setIsActiveMenu, ...props}) => {
 	const menuRef = createRef();
+	const router = useRouter();
+	const [languageFile, setLanguageFile] = useState();
+	
+	useEffect(() => {
+		if (router.locale) {
+			import(`../../lang/${router.locale}.json`).then((language) => setLanguageFile(language.default));
+		}
+	}, [setLanguageFile, router.locale]);
 	
 	useOutsideClickHandler(menuRef, isActiveMenu, setIsActiveMenu);
 	
 	return (
 		<div {...props} className={`${styles.Menu} ${isActiveMenu ? styles.Menu_active : ''}`} ref={menuRef}>
 			<header className={styles.Menu__header}>
-				<Button.Secondary>Авторизация</Button.Secondary>
-				<Button.Primary>Регистрация</Button.Primary>
+				<Button.Secondary>{languageFile?.['menu']?.['authorization-button']}</Button.Secondary>
+				<Button.Primary>{languageFile?.['menu']?.['registration-button']}</Button.Primary>
 			</header>
 			<div className={styles.Menu__search}>
 				<label>
-					<input type="text" placeholder="Например, экскурсии по Москва-реке"/>
+					<input type="text" placeholder={languageFile?.['menu']?.['example-input']}/>
 					<svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
 						<path d="M10.8989 10.3301L15.1513 15.1041C15.2248 15.1866 15.2151 15.3102 15.1296 15.3801L14.4237 15.9582C14.3383 16.0282 14.2094 16.0181 14.136 15.9356L9.88356 11.1616L10.8989 10.3301Z" fill="#B9BFC5"/>
 						<path d="M12.469 6.21694C12.469 9.35466 9.80973 11.9339 6.48451 11.9339C3.15929 11.9339 0.5 9.35466 0.5 6.21694C0.5 3.07921 3.15929 0.5 6.48451 0.5C9.80973 0.5 12.469 3.07921 12.469 6.21694Z" stroke="#B9BFC5"/>
 					</svg>
 				</label>
 				<span>
-					Последние запросы: <b>водные экскурсии, экскурсия в музее</b>
+					{languageFile?.['menu']?.['latest-queries']}: <b>водные экскурсии, экскурсия в музее</b>
 				</span>
 			</div>
 			<Navigation/>
 			<footer className={styles.Menu__footer}>
 				<div className={styles.Menu__title}>
-					<h4>Часто посещают в Москве</h4>
-					<Link href="#"><a>Изменить город</a></Link>
+					<h4>{languageFile?.['menu']?.['places-title']}</h4>
+					<Link href="#"><a>{languageFile?.['menu']?.['places-city-change']}</a></Link>
 				</div>
 				<ul className={styles.Menu__places}>
 					<li>
--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true
+	reactStrictMode: true,
+	i18n: {
+		locales: ['default', 'ru', 'en'],
+		defaultLocale: 'default',
+		localeDetection: true
+	}
 };
 
 module.exports = nextConfig;
