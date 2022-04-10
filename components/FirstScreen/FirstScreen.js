5e9f26387b8376f134496b7c48b7a0dd174a4a5a
eleskin <eleskin1201@gmail.com>
2022-04-10 19:09:49 +0300

    feature: Added translation files

--- components/FirstScreen/FirstScreen.js
+++ components/FirstScreen/FirstScreen.js
@@ -14,9 +14,15 @@ const FirstScreen = () => {
 	const [value, onChange] = useState(new Date());
 	const [currentLocale, setCurrentLocale] = useState('ru');
 	const {asPath} = router;
-	
 	const [date, setDate] = useState(new Date(Date.parse(value.toString())));
 	const [currentDate, setCurrentDate] = useState(`${date.getDate().toString().length === 2 ? date.getDate() : `0${date.getDate()}`}-${date.getMonth().toString().length === 2 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`}-${date.getFullYear()}`);
+	const [languageFile, setLanguageFile] = useState();
+	
+	useEffect(() => {
+		if (router.locale) {
+			import(`../../languages/${router.locale}.json`).then((language) => setLanguageFile(language.default));
+		}
+	}, [setLanguageFile, router.locale]);
 	
 	useEffect(() => {
 		setDate(new Date(Date.parse(value.toString())));
@@ -66,8 +72,8 @@ const FirstScreen = () => {
 				<RegionSelect mode="first-screen"/>
 			</header>
 			<div className={styles.FirstScreen__title}>
-				<span>Дольше путешествие - больше открытий</span>
-				<h1>Экскурсии и туры с лучшими гидами по Москве</h1>
+				<span>{languageFile?.['first-screen']?.['subtitle']}</span>
+				<h1>{languageFile?.['first-screen']?.['title']} Москве</h1>
 			</div>
 			<div className={styles.FirstScreen__search} ref={searchRef}>
 				<div className={`${styles.FirstScreen__calendar} ${isVisibleCalendar ? styles.FirstScreen__calendar_active : ''}`}>
@@ -87,7 +93,7 @@ const FirstScreen = () => {
 						onFocus={handleFocusInput}
 					/>
 					<Image src="/assets/images/first-screen/first-screen-calendar.svg" width={15} height={14} alt=""/>
-					<Button.Primary small={true}>Найти</Button.Primary>
+					<Button.Primary small={true}>{languageFile?.['first-screen']?.['search-button']}</Button.Primary>
 				</label>
 			</div>
 			<UpcomingExcursions/>
