9948f933268cf18035668505acb6f3c6862cdc3f
eleskin <eleskin1201@gmail.com>
2022-04-12 11:53:53 +0300

    feature: Added PopularCategories component

---
+++ components/PopularCategoriesCard/PopularCategoriesCard.js
@@ -0,0 +1,22 @@
+import styles from './PopularCategoriesCard.module.scss';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const PopularCategoriesCard = ({price, title, count, image}) => {
+	return (
+		<Link href="#">
+			<div className={styles.PopularCategoriesCard}>
+				<div className={styles.PopularCategoriesCard__image}>
+					<Image src={image} width={320} height={150} alt="" layout="responsive"/>
+					<span className={styles.PopularCategoriesCard__price}>от {price} ₽</span>
+				</div>
+				<div className={styles.PopularCategoriesCard__info}>
+					<h5>{title}</h5>
+					<span>более {count} предложений</span>
+				</div>
+			</div>
+		</Link>
+	);
+};
+
+export default PopularCategoriesCard;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import styles from '/styles/Home.module.scss';
 import FirstScreen from '../components/FirstScreen/FirstScreen';
+import PopularCategories from '../components/PopularCategories/PopularCategories';
 
 const Home = () => {
 	return (
 		<div className={styles.Home}>
 			<FirstScreen/>
+			<PopularCategories/>
 		</div>
 	);
 };
