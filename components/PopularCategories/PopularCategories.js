6676144805e57d9b0ded4b3525d9f519b20a974c
eleskin <eleskin1201@gmail.com>
2022-04-12 12:15:52 +0300

    feature: Added translate files

--- components/PopularCategories/PopularCategories.js
+++ components/PopularCategories/PopularCategories.js
@@ -1,3 +1,5 @@
+import {useRouter} from 'next/router';
+import {useEffect, useState} from 'react';
 import Container from '../Container/Container';
 import PopularCategoriesCard from '../PopularCategoriesCard/PopularCategoriesCard';
 import Title from '../Title/Title';
@@ -5,10 +7,19 @@ import styles from './PopularCategories.module.scss';
 import Link from 'next/link';
 
 const PopularCategories = () => {
+	const router = useRouter();
+	const [languageFile, setLanguageFile] = useState();
+	
+	useEffect(() => {
+		if (router.locale) {
+			import(`../../languages/${router.locale}.json`).then((language) => setLanguageFile(language.default));
+		}
+	}, [setLanguageFile, router.locale]);
+	
 	return (
 		<div className={styles.PopularCategories}>
 			<Container>
-				<Title>Популярные категории</Title>
+				<Title>{languageFile?.['popular-categories']?.['title']}</Title>
 				<div className={styles.PopularCategories__cards}>
 					<PopularCategoriesCard
 						price={550}
@@ -32,7 +43,7 @@ const PopularCategories = () => {
 				<footer className={styles.PopularCategories__footer}>
 					<Link href="#">
 						<a>
-							Каталог экскурсий
+							{languageFile?.['popular-categories']?.['catalog-link']}
 							<svg width="18" height="8" viewBox="0 0 18 8" fill="none" xmlns="http://www.w3.org/2000/svg">
 								<path d="M17.3536 4.35355C17.5488 4.15829 17.5488 3.84171 17.3536 3.64645L14.1716 0.464466C13.9763 0.269204 13.6597 0.269204 13.4645 0.464466C13.2692 0.659728 13.2692 0.976311 13.4645 1.17157L16.2929 4L13.4645 6.82843C13.2692 7.02369 13.2692 7.34027 13.4645 7.53553C13.6597 7.7308 13.9763 7.7308 14.1716 7.53553L17.3536 4.35355ZM0 4.5H17V3.5H0V4.5Z" fill="#212121"/>
 							</svg>
--- components/PopularCategoriesCard/PopularCategoriesCard.js
+++ components/PopularCategoriesCard/PopularCategoriesCard.js
@@ -1,18 +1,33 @@
+import {useRouter} from 'next/router';
+import {useEffect, useState} from 'react';
 import styles from './PopularCategoriesCard.module.scss';
 import Image from 'next/image';
 import Link from 'next/link';
 
 const PopularCategoriesCard = ({price, title, count, image}) => {
+	const router = useRouter();
+	const [languageFile, setLanguageFile] = useState();
+	
+	useEffect(() => {
+		if (router.locale) {
+			import(`../../languages/${router.locale}.json`).then((language) => setLanguageFile(language.default));
+		}
+	}, [setLanguageFile, router.locale]);
+	
 	return (
 		<Link href="#">
 			<div className={styles.PopularCategoriesCard}>
 				<div className={styles.PopularCategoriesCard__image}>
 					<Image src={image} width={390} height={230} alt="" layout="responsive"/>
-					<span className={styles.PopularCategoriesCard__price}>от {price} ₽</span>
+					<span className={styles.PopularCategoriesCard__price}>
+						{languageFile?.['popular-categories-card']?.['price']} {price} ₽
+					</span>
 				</div>
 				<div className={styles.PopularCategoriesCard__info}>
 					<h5>{title}</h5>
-					<span>более {count} предложений</span>
+					<span>
+						{languageFile?.['popular-categories-card']?.['count-begin']} {count} {languageFile?.['popular-categories-card']?.['count-end']}
+					</span>
 				</div>
 			</div>
 		</Link>
