54995882ba076a8f40bd712b8b41add8022a8902
eleskin <eleskin1201@gmail.com>
2022-04-13 22:11:31 +0300

    feature: Added Advantages component

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import styles from '/styles/Home.module.scss';
+import Advantages from '../components/Advantages/Advantages';
 import FirstScreen from '../components/FirstScreen/FirstScreen';
 import PopularCategories from '../components/PopularCategories/PopularCategories';
 import SpecialOffers from '../components/SpecialOffers/SpecialOffers';
@@ -9,6 +10,7 @@ const Home = () => {
 			<FirstScreen/>
 			<PopularCategories/>
 			<SpecialOffers/>
+			<Advantages/>
 		</div>
 	);
 };
