822a59addd314d0b97773c2ce17f9f903873a097
eleskin <eleskin1201@gmail.com>
2022-04-14 21:02:09 +0300

    feature: Added language files

--- components/LimitedOpportunities/LimitedOpportunities.js
+++ components/LimitedOpportunities/LimitedOpportunities.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
-import {useState} from 'react';
+import {useRouter} from 'next/router';
+import {useEffect, useState} from 'react';
 import CardSlider from '../CardSlider/CardSlider';
 import Container from '../Container/Container';
 import ExcursionCard from '../ExcursionCard/ExcursionCard';
@@ -9,6 +10,14 @@ import styles from './LimitedOpportunities.module.scss';
 const LimitedOpportunities = () => {
 	const [activeSlide, setActiveSlide] = useState(0);
 	const [isDisabledNextButton, setIsDisabledNextButton] = useState(false);
+	const router = useRouter();
+	const [languageFile, setLanguageFile] = useState();
+	
+	useEffect(() => {
+		if (router.locale) {
+			import(`../../languages/${router.locale}.json`).then((language) => setLanguageFile(language.default));
+		}
+	}, [setLanguageFile, router.locale]);
 	
 	const slides = [
 		{},
@@ -43,8 +52,8 @@ const LimitedOpportunities = () => {
 			<Container>
 				<div className={styles.LimitedOpportunities__title}>
 					<div>
-						<Title style={{marginBottom: 0}}>С ограниченными возможностями</Title>
-						<span>Предоставляем подборку лучших экскурсий, которые будут комфортны для людей с ограниченными возможностями</span>
+						<Title style={{marginBottom: 0}}>{languageFile?.['limited-opportunities']?.['title']}</Title>
+						<span>{languageFile?.['limited-opportunities']?.['subtitle']}</span>
 					</div>
 					<div className={styles.LimitedOpportunities__buttons}>
 						<button
@@ -79,7 +88,7 @@ const LimitedOpportunities = () => {
 				<footer className={styles.LimitedOpportunities__footer}>
 					<Link href="#">
 						<a>
-							Все экскурсии в данной категории
+							{languageFile?.['limited-opportunities']?.['link']}
 							<svg width="18" height="8" viewBox="0 0 18 8" fill="none" xmlns="http://www.w3.org/2000/svg">
 								<path d="M17.3536 4.35355C17.5488 4.15829 17.5488 3.84171 17.3536 3.64645L14.1716 0.464466C13.9763 0.269204 13.6597 0.269204 13.4645 0.464466C13.2692 0.659728 13.2692 0.976311 13.4645 1.17157L16.2929 4L13.4645 6.82843C13.2692 7.02369 13.2692 7.34027 13.4645 7.53553C13.6597 7.7308 13.9763 7.7308 14.1716 7.53553L17.3536 4.35355ZM0 4.5H17V3.5H0V4.5Z" fill="#212121"/>
 							</svg>
