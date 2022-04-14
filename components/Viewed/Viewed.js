7708ebec27fdb7f3eda39a0efc576b20c5b64e3b
eleskin <eleskin1201@gmail.com>
2022-04-14 19:29:58 +0300

    feature: Added Viewed component

--- components/Viewed/Viewed.js
+++ components/Viewed/Viewed.js
@@ -1,14 +1,33 @@
 import Container from '../Container/Container';
+import ExcursionCard from '../ExcursionCard/ExcursionCard';
 import Title from '../Title/Title';
 import styles from './Viewed.module.scss';
 
 const Viewed = () => {
+	const slides = [
+		{},
+		{},
+		{},
+		{},
+	];
+	
+	const slidesList = slides.map((slide, index) => (
+		<ExcursionCard
+			viewed={true}
+			key={index}
+		/>
+	));
+	
 	return (
 		<div className={styles.Viewed}>
 			<Container>
 				<Title>Вы смотрели</Title>
-				
 			</Container>
+			<div className={styles.Viewed__slider}>
+				<div className={styles.Viewed__container}>
+					{slidesList}
+				</div>
+			</div>
 		</div>
 	);
 };
