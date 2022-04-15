fd2b87f1da1f7d609f5b9c57154f91868fd13af1
eleskin <eleskin1201@gmail.com>
2022-04-15 16:39:41 +0300

    fix: Locale detection

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,21 +2,17 @@ import {NextResponse} from 'next/server';
 
 const PUBLIC_FILE = /\.(.*)$/;
 
-const stripDefaultLocale = (str) => {
-	return str.replace('/default', '');
-};
-
 export function middleware(request) {
 	const shouldHandleLocale =
 		!PUBLIC_FILE.test(request.nextUrl.pathname) &&
 		!request.nextUrl.pathname.includes('/api/') &&
 		request.nextUrl.locale === 'default';
 	
-	return shouldHandleLocale
-		? NextResponse.redirect(
-			`/ru${stripDefaultLocale(request.nextUrl.pathname)}${
-				request.nextUrl.search
-			}`,
-		)
-		: undefined;
+	if (shouldHandleLocale) {
+		const url = request.nextUrl.clone();
+		url.pathname = `/ru${request.nextUrl.pathname}`;
+		return NextResponse.redirect(url);
+	}
+	
+	return undefined;
 }
