220f2bd9d5a523c77efe2f7a04b2ac7cd41f1914
eleskin <eleskin1201@gmail.com>
2022-05-18 10:22:58 +0300

    feature: Added Order container

--- pages/order.js
+++ pages/order.js
@@ -1,4 +1,5 @@
 import Alert from '../components/Alert/Alert';
+import CardContainer from '../components/CardContainer/CardContainer';
 import Container from '../components/Container/Container';
 import styles from '../styles/Order.module.scss';
 
@@ -6,10 +7,19 @@ const Order = () => {
 	return (
 		<div className={styles.Order}>
 			<Container isMainPage={true}>
-				<Alert
-					title="Предупреждение!"
-					text="При покупке билета на ночную экскурсию Вам нужно выбрать текущую дату. Например если вы хотите купить билет на экскурсию 07.04.2022 в 00:35 вы должны выбрать дату - 06.04.2022."
-				/>
+				<div className={styles.Order__container}>
+					<div>
+						<Alert
+							title="Предупреждение!"
+							text="При покупке билета на ночную экскурсию Вам нужно выбрать текущую дату. Например если вы хотите купить билет на экскурсию 07.04.2022 в 00:35 вы должны выбрать дату - 06.04.2022."
+						/>
+						<CardContainer padding={16}>
+							<div className={styles.Order__cart}>
+								<h2>Корзина</h2>
+							</div>
+						</CardContainer>
+					</div>
+				</div>
 			</Container>
 		</div>
 	);
