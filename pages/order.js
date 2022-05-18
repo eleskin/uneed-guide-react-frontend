a8d7a7e5627fc9d3f80a20107afb764a18035824
eleskin <eleskin1201@gmail.com>
2022-05-18 16:19:27 +0300

    feature: Added Order total component

--- pages/order.js
+++ pages/order.js
@@ -1,4 +1,6 @@
+import Link from 'next/link';
 import Alert from '../components/Alert/Alert';
+import Button from '../components/Button/Button';
 import CardContainer from '../components/CardContainer/CardContainer';
 import Cart from '../components/Cart/Cart';
 import Container from '../components/Container/Container';
@@ -16,6 +18,28 @@ const Order = () => {
 						/>
 						<CardContainer padding={16}>
 							<Cart/>
+						</CardContainer>
+					</div>
+					<div className={styles.Order__sidebar}>
+						<CardContainer padding={16}>
+							<div className={styles.Order__total}>
+								<header>
+									<h3>Итого</h3>
+									<strong>1 560 ₽</strong>
+								</header>
+								<div>
+									<span>Билетов, 4 шт.</span>
+									<span>2 100 ₽</span>
+								</div>
+								<div>
+									<span>Скидка</span>
+									<span>560 ₽</span>
+								</div>
+								<Button.Primary>Оформить заказ</Button.Primary>
+								<a>
+									Согласен с условиями <Link href="#"><a>пользования торговой площадкой</a></Link> и <Link href="#"><a>условия возврата</a></Link>
+								</a>
+							</div>
 						</CardContainer>
 					</div>
 				</div>
