13f213a7a66f08113388eda9dfa384ef67017dbf
eleskin <eleskin1201@gmail.com>
2022-05-20 00:34:15 +0300

    feature: Added Order advantages

--- pages/order.js
+++ pages/order.js
@@ -5,6 +5,7 @@ import Button from '../components/Button/Button';
 import CardContainer from '../components/CardContainer/CardContainer';
 import Cart from '../components/Cart/Cart';
 import Container from '../components/Container/Container';
+import Help from '../components/Help/Help';
 import styles from '../styles/Order.module.scss';
 import Form from '../ui/Form/Form';
 
@@ -97,6 +98,25 @@ const Order = () => {
 								</p>
 							</div>
 						</CardContainer>
+						<ul className={styles.Order__advantages}>
+							<li>
+								<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<path d="M10 0C4.5 0 0 4.5 0 10C0 15.5 4.5 20 10 20C15.5 20 20 15.5 20 10C20 4.5 15.5 0 10 0ZM14.2 8.3L9.4 13.1C9 13.5 8.4 13.5 8 13.1L5.8 10.9C5.4 10.5 5.4 9.9 5.8 9.5C6.2 9.1 6.8 9.1 7.2 9.5L8.7 11L12.8 6.9C13.2 6.5 13.8 6.5 14.2 6.9C14.6 7.3 14.6 7.9 14.2 8.3Z" fill="#F0515D"/>
+								</svg>
+								<span>Предоплата не требуется, платите на месте</span>
+							</li>
+							<li>
+								<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<path d="M10 0C4.5 0 0 4.5 0 10C0 15.5 4.5 20 10 20C15.5 20 20 15.5 20 10C20 4.5 15.5 0 10 0ZM14.2 8.3L9.4 13.1C9 13.5 8.4 13.5 8 13.1L5.8 10.9C5.4 10.5 5.4 9.9 5.8 9.5C6.2 9.1 6.8 9.1 7.2 9.5L8.7 11L12.8 6.9C13.2 6.5 13.8 6.5 14.2 6.9C14.6 7.3 14.6 7.9 14.2 8.3Z" fill="#F0515D"/>
+								</svg>
+								<span>Безопасные платежи. Мы не храним данные ваших платежных систем</span>
+							</li>
+						</ul>
+						<Help links={[
+							{text: 'Частозадаваемые вопросы', href: '#'},
+							{text: 'Задать вопрос по экскурсии', href: '#'},
+							{text: 'Контакты', href: '#'},
+						]}/>
 					</div>
 				</div>
 			</Container>
