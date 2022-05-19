ed1490f77b5a3f37f0d62a708bd3b847278072bd
eleskin <eleskin1201@gmail.com>
2022-05-19 18:44:59 +0300

    feature: Added total calculating

--- components/Cart/Cart.js
+++ components/Cart/Cart.js
@@ -1,10 +1,130 @@
-import {useState} from 'react';
+import {format, isToday, isTomorrow} from 'date-fns';
+import {ru} from 'date-fns/locale';
+import {useRouter} from 'next/router';
+import {createRef, useState} from 'react';
 import Image from 'next/image';
+import Calendar from 'react-calendar';
+import {useOutsideClickHandler} from '../../utils/hooks';
 import styles from './Cart.module.scss';
 import Form from '../../ui/Form/Form';
 
-const Cart = () => {
+const ticketsCountInitial = [];
+
+const Cart = ({order, setTotalSumWithoutDiscount, setTotalSumWithDiscount}) => {
+	const router = useRouter();
+	const [isVisibleCalendar, setIsVisibleCalendar] = useState(false);
+	const [dateValue, setDateValue] = useState();
 	const [currentJetty, setCurrentJetty] = useState('ст. м. Маяковская, театр сатиры');
+	const [ticketsCount, setTicketsCount] = useState(ticketsCountInitial);
+	const [total] = useState(0);
+	
+	const getTotal = (total, ticketsCount, tickets, field) => {
+		return tickets.reduce((accumulator, ticket, index) => {
+			return accumulator + ticket[field] * (ticketsCount[index] || 0);
+		}, 0);
+	};
+	
+	const [dates] = useState([
+		new Date(2022, 3, 30),
+		new Date(2022, 4, 1),
+		new Date(2022, 4, 2),
+		new Date(2022, 4, 3),
+	]);
+	const [activeDate, setActiveDate] = useState(dates[0]);
+	const datesList = dates.map((date, index) => {
+		if (index < 3) {
+			return (
+				<button
+					className={`${styles.Cart__date} ${activeDate === date ? styles.Cart__date_active : ''}`}
+					onClick={() => setActiveDate(date)}
+					key={index}
+				>
+					<em>{isToday(date) ? 'Сегодня' : isTomorrow(date) ? 'Завтра' : format(date, 'EEEE', {locale: ru})}</em>
+					<span>{format(date, 'dd MMMM', {locale: ru})}</span>
+				</button>
+			);
+		} else if (index === 2 && dates.length === 3) {
+			return (
+				<button
+					className={`${styles.Cart__date} ${activeDate === date ? styles.Cart__date_active : ''}`}
+					onClick={() => setActiveDate(date)}
+					key={index}
+				>
+					<em>{isToday(date) ? 'Сегодня' : isTomorrow(date) ? 'Завтра' : format(date, 'EEEE', {locale: ru})}</em>
+					<span>{format(date, 'dd MMMM', {locale: ru})}</span>
+				</button>
+			);
+		}
+	});
+	
+	const [times] = useState([
+		'12:45',
+		'13:00',
+		'13:15',
+		'13:30',
+	]);
+	const [activeTime, setActiveTime] = useState(times[0]);
+	const timesList = times.map((time, index) => (
+		<button
+			className={`${styles.Cart__time} ${activeTime === time ? styles.Cart__time_active : ''}`}
+			onClick={() => setActiveTime(time)}
+			key={index}
+		>
+			{time}
+		</button>
+	));
+	
+	const datesRef = createRef();
+	
+	useOutsideClickHandler(datesRef, isVisibleCalendar, setIsVisibleCalendar);
+	
+	const tickets = order[0].tickets;
+	const ticketsList = tickets.map((ticket, index) => (
+		<div className={styles.Cart__ticket}>
+			<div>
+				<em>{ticket.title}</em>
+				<div>
+					<span>Цена 1 билета</span>
+					<strong>{ticket.priceAmount} ₽</strong>
+				</div>
+			</div>
+			<div>
+				<button
+					onClick={() => {
+						ticketsCountInitial[index] = ticketsCountInitial[index] - 1;
+						setTicketsCount([...ticketsCountInitial]);
+						
+						setTotalSumWithoutDiscount(getTotal(total, ticketsCountInitial, tickets, 'price'));
+						setTotalSumWithDiscount(getTotal(total, ticketsCountInitial, tickets, 'priceAmount'));
+					}}
+					disabled={ticketsCount[index] <= 0}
+				>
+					<svg width="11" height="1" viewBox="0 0 11 1" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<rect y="1" width="1" height="11" rx="0.5" transform="rotate(-90 0 1)" fill="#221818"/>
+					</svg>
+				</button>
+				<output>{ticketsCount[index] || 0}</output>
+				<button
+					onClick={() => {
+						if (!ticketsCountInitial[index]) {
+							ticketsCountInitial[index] = 0;
+						}
+						
+						ticketsCountInitial[index] = ticketsCountInitial[index] + 1;
+						setTicketsCount([...ticketsCountInitial]);
+						
+						setTotalSumWithoutDiscount(getTotal(total, ticketsCountInitial, tickets, 'price'));
+						setTotalSumWithDiscount(getTotal(total, ticketsCountInitial, tickets, 'priceAmount'));
+					}}
+				>
+					<svg width="11" height="11" viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<rect x="5" width="1" height="11" rx="0.5" fill="#221818"/>
+						<rect y="6" width="1" height="11" rx="0.5" transform="rotate(-90 0 6)" fill="#221818"/>
+					</svg>
+				</button>
+			</div>
+		</div>
+	));
 	
 	return (
 		<div className={styles.Cart}>
@@ -40,102 +160,39 @@ const Cart = () => {
 					/>
 				</div>
 				<h3>Выберите дату и время экскурсии</h3>
-				<div className={styles.Cart__dates}>
-					<button className={`${styles.Cart__date} ${styles.Cart__date_active}`}>
-						<em>Сегодня</em>
-						<span>30 марта</span>
-					</button>
-					<button className={styles.Cart__date}>
-						<em>Завтра</em>
-						<span>31 марта</span>
-					</button>
-					<button className={styles.Cart__date}>
-						<em>Понедельник</em>
-						<span>1 апреля</span>
-					</button>
-					<button className={styles.Cart__date}>
-						<em>Вторник</em>
-						<span>2 апреля</span>
-					</button>
+				<div className={styles.Cart__dates} ref={datesRef}>
+					<div>
+						{datesList}
+						{dates.length > 3 && (
+							<button
+								className={`${styles.Cart__date} ${styles.Cart__date_more}`}
+								onClick={() => setIsVisibleCalendar((prevState) => !prevState)}
+							>
+								<svg width="16" height="13" viewBox="0 0 21 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<path d="M17.5139 2.00009H18.8889C19.4412 2.00009 19.8889 2.44781 19.8889 3.00009V17C19.8889 17.5523 19.4412 18 18.8889 18H1.88892C1.33663 18 0.888916 17.5523 0.888916 17V3.00009C0.888916 2.44781 1.33663 2.00009 1.88892 2.00009H3.26392M12.7639 2.00009H10.3889H8.01392" stroke="#F0515D"/>
+									<rect x="4.88892" width="1.125" height="3.99998" rx="0.5625" fill="#F0515D"/>
+									<rect x="14.8889" width="1.125" height="3.99998" rx="0.5625" fill="#F0515D"/>
+								</svg>
+								<em>Выбрать дату</em>
+							</button>
+						)}
+					</div>
+					<div className={`${styles.Cart__calendar} ${isVisibleCalendar ? styles.Cart__calendar_active : ''}`}>
+						<Calendar
+							locale={router.locale}
+							value={dateValue}
+							onChange={setDateValue}
+							onClickDay={() => setIsVisibleCalendar(false)}
+						/>
+					</div>
 				</div>
 				<h3>Время отправления</h3>
 				<div className={styles.Cart__times}>
-					<button className={`${styles.Cart__time} ${styles.Cart__time_active}`}>12:45</button>
-					<button className={styles.Cart__time}>12:45</button>
-					<button className={styles.Cart__time}>12:45</button>
-					<button className={styles.Cart__time}>12:45</button>
+					{timesList}
 				</div>
 				<h3>Выберите билеты</h3>
 				<div className={styles.Cart__tickets}>
-					<div className={styles.Cart__ticket}>
-						<div>
-							<em>Взрослый билет</em>
-							<div>
-								<span>Цена 1 билета</span>
-								<strong>700 ₽</strong>
-							</div>
-						</div>
-						<div>
-							<button>
-								<svg width="11" height="1" viewBox="0 0 11 1" fill="none" xmlns="http://www.w3.org/2000/svg">
-									<rect y="1" width="1" height="11" rx="0.5" transform="rotate(-90 0 1)" fill="#221818"/>
-								</svg>
-							</button>
-							<output>2</output>
-							<button>
-								<svg width="11" height="11" viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-									<rect x="5" width="1" height="11" rx="0.5" fill="#221818"/>
-									<rect y="6" width="1" height="11" rx="0.5" transform="rotate(-90 0 6)" fill="#221818"/>
-								</svg>
-							</button>
-						</div>
-					</div>
-					<div className={styles.Cart__ticket}>
-						<div>
-							<em>Детский билет</em>
-							<div>
-								<span>Цена 1 билета</span>
-								<strong>450 ₽</strong>
-							</div>
-						</div>
-						<div>
-							<button>
-								<svg width="11" height="1" viewBox="0 0 11 1" fill="none" xmlns="http://www.w3.org/2000/svg">
-									<rect y="1" width="1" height="11" rx="0.5" transform="rotate(-90 0 1)" fill="#221818"/>
-								</svg>
-							</button>
-							<output>0</output>
-							<button>
-								<svg width="11" height="11" viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-									<rect x="5" width="1" height="11" rx="0.5" fill="#221818"/>
-									<rect y="6" width="1" height="11" rx="0.5" transform="rotate(-90 0 6)" fill="#221818"/>
-								</svg>
-							</button>
-						</div>
-					</div>
-					<div className={styles.Cart__ticket}>
-						<div>
-							<em>Взрослый билет (с ланчем)</em>
-							<div>
-								<span>Цена 1 билета</span>
-								<strong>880 ₽</strong>
-							</div>
-						</div>
-						<div>
-							<button>
-								<svg width="11" height="1" viewBox="0 0 11 1" fill="none" xmlns="http://www.w3.org/2000/svg">
-									<rect y="1" width="1" height="11" rx="0.5" transform="rotate(-90 0 1)" fill="#221818"/>
-								</svg>
-							</button>
-							<output>2</output>
-							<button>
-								<svg width="11" height="11" viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-									<rect x="5" width="1" height="11" rx="0.5" fill="#221818"/>
-									<rect y="6" width="1" height="11" rx="0.5" transform="rotate(-90 0 6)" fill="#221818"/>
-								</svg>
-							</button>
-						</div>
-					</div>
+					{ticketsList}
 				</div>
 			</div>
 		</div>
--- pages/order.js
+++ pages/order.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import {useState} from 'react';
 import Alert from '../components/Alert/Alert';
 import Button from '../components/Button/Button';
 import CardContainer from '../components/CardContainer/CardContainer';
@@ -6,7 +7,22 @@ import Cart from '../components/Cart/Cart';
 import Container from '../components/Container/Container';
 import styles from '../styles/Order.module.scss';
 
+
 const Order = () => {
+	const [totalSumWithoutDiscount, setTotalSumWithoutDiscount] = useState(0);
+	const [totalSumWithDiscount, setTotalSumWithDiscount] = useState(0);
+	
+	const [order] = useState([
+		{
+			tickets: [
+				{title: 'Взрослый билет', price: 800, priceAmount: 700},
+				{title: 'Детский билет', price: 550, priceAmount: 450},
+				{title: 'Взрослый билет (с ланчем)', price: 980, priceAmount: 880},
+			]
+		}
+	]);
+	
+	
 	return (
 		<div className={styles.Order}>
 			<Container isMainPage={true}>
@@ -17,7 +33,11 @@ const Order = () => {
 							text="При покупке билета на ночную экскурсию Вам нужно выбрать текущую дату. Например если вы хотите купить билет на экскурсию 07.04.2022 в 00:35 вы должны выбрать дату - 06.04.2022."
 						/>
 						<CardContainer padding={16}>
-							<Cart/>
+							<Cart
+								order={order}
+								setTotalSumWithoutDiscount={setTotalSumWithoutDiscount}
+								setTotalSumWithDiscount={setTotalSumWithDiscount}
+							/>
 						</CardContainer>
 					</div>
 					<div className={styles.Order__sidebar}>
@@ -25,20 +45,20 @@ const Order = () => {
 							<div className={styles.Order__total}>
 								<header>
 									<h3>Итого</h3>
-									<strong>1 560 ₽</strong>
+									<strong>{totalSumWithDiscount} ₽</strong>
 								</header>
 								<div>
 									<span>Билетов, 4 шт.</span>
-									<span>2 100 ₽</span>
+									<span>{totalSumWithoutDiscount} ₽</span>
 								</div>
 								<div>
 									<span>Скидка</span>
-									<span>560 ₽</span>
+									<span>{totalSumWithoutDiscount - totalSumWithDiscount} ₽</span>
 								</div>
 								<Button.Primary>Оформить заказ</Button.Primary>
-								<a>
+								<p>
 									Согласен с условиями <Link href="#"><a>пользования торговой площадкой</a></Link> и <Link href="#"><a>условия возврата</a></Link>
-								</a>
+								</p>
 							</div>
 						</CardContainer>
 					</div>
