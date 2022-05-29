7779aae8d2dbd104b84766561805c0811bb98902
eleskin <eleskin1201@gmail.com>
2022-05-29 12:25:29 +0300

    fix: Breadcrumbs path

--- components/Breadcrumbs/Breadcrumbs.js
+++ components/Breadcrumbs/Breadcrumbs.js
@@ -5,18 +5,22 @@ import styles from './Breadcrumbs.module.scss';
 
 const Breadcrumbs = () => {
 	const router = useRouter();
-	const pages = router.asPath.split('/');
+	const pages = router.pathname.split('/');
+	pages.push('');
+	
+	const indexCity = pages.indexOf('[city]');
+	pages.splice(indexCity, 1);
 	
 	return (
 		<div className={styles.Breadcrumbs}>
 			{pages.map((page, index) => index !== pages.length - 1 && (
-				<Link href={index < pages.length - 1 ?`/${page}` : ''} key={index}>
+				<Link href={index < pages.length - 1 ? `/${page}` : ''} key={index}>
 					<span>
 						<a>
 							{
 								page === '' ?
-								getTranslatedPageName('home', router.locale) :
-								getTranslatedPageName(page, router.locale)
+									getTranslatedPageName('home', router.locale) :
+									getTranslatedPageName(page, router.locale)
 							}
 						</a>
 					</span>
